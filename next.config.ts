175df23a53db9fbc08e43a881890f889fb655253
dwayne <DwayneKasperLamar@gmail.com>
2025-01-15 10:36:51 +0100

    product page setup

--- next.config.ts
+++ next.config.ts
@@ -11,4 +11,13 @@ const nextConfig: NextConfig = {
   }
 };
 
+// experimental:{
+//   ppr: 'incremental'
+// },
+//   devIndicators:{
+//     appIsrStatus: true,
+//     buildActivity: true,
+//     buildActivityPosition: 'bottom-right'
+//   }
+
 export default nextConfig;
